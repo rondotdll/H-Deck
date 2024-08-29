799d0f273ba16d0ae134a26c6f40c840f0489ec3
rondotdll <hidden>
2024-08-29 15:41:06 -0400

    Added demo code

--- src/main.ino
+++ src/main.ino
@@ -22,12 +22,23 @@ void setup() {
     powerTFT();
 
     // Create and draw a window
-    SGui::UIWindow window(0, 0, "Main Window", 0, 0, TFT_RED);
+    SGui::UIWindow window(0, 0, "H-DECK PROTOTYPE FIRMWARE", 0, 0, TFT_RED);
     window.draw();
 
     // Create and draw a label
-    SGui::UILabel label(5, 25, "Hello, World!");
-    label.draw();
+    SGui::UILabel platform_label(5, 25, "PLATFORM: ESP32-S3 WiFi Enabled Chipset");
+    SGui::UILabel based_on_label(5, 50, "DEVICE: T-DECK-920M");
+    SGui::UILabel clockspeed_label(5, 75, "CPU: Dual Core XTensa LX7 MCU @ 240MHz");
+    platform_label.draw();
+    based_on_label.draw();
+    clockspeed_label.draw();
+
+    SGui::UILabel check_label(5, 100, "CHECK", 0, 0, 4);
+    SGui::UILabel your_label(5, 150, "YOUR", 0, 0, 4);
+    SGui::UILabel wifi_label(5, 200, "WIFI", 0, 0, 4);
+    check_label.draw();
+    your_label.draw();
+    wifi_label.draw();
 
     // // Create and draw a button with an action
     // button = new SGui::UIButton(20, 80, "Click Me", []() {
